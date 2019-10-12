305ff86a54c028ece4f9efd13e20a5abf04fb1dd
Lennox Bampoe-Addo <LBampoe-Addo@spartaglobal.com>
2019-10-12 23:37:45 +0100

    created jenkins in provisions

---
+++ environment/provision.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install openjdk-8-jdk -y
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt update
+
+sudo apt install jenkins -y
+
+
+sudo ufw enable -y
+
+sudo ufw default deny
+
+sudo iptables -L
+
+
+sudo ufw allow 8080
+
+sudo ufw enable -y
+
+sudo ufw default deny
+
+sudo ufw allow 8080
